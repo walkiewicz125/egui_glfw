259c1ab7e174fc9dda862057c46e9e172595b6fd
= <=>
2024-04-17 18:42:39 +0200

    Split exampl into more usable structs. Easier to start new project based on this example

--- examples/demo/main.rs
+++ examples/demo/main.rs
@@ -1,7 +1,10 @@
+use std::sync::mpsc::Receiver;
 use std::time::Instant;
 
+use egui::{InnerResponse, Ui, WidgetText};
 use egui_glfw::egui::{vec2, Color32, Image, Pos2, Rect};
 use egui_glfw::glfw::Context;
+use glfw::{Glfw, WindowEvent, WindowHint};
 use triangle::TriangleWidget;
 
 const SCREEN_WIDTH: u32 = 800;
@@ -11,48 +14,147 @@ const PIC_HEIGHT: i32 = 192;
 
 mod triangle;
 
+pub struct AppWindow {
+    pub glfw: Glfw,
+    pub window: glfw::Window,
+    pub events: Receiver<(f64, WindowEvent)>,
+    pub painter: egui_glfw::Painter,
+    pub egui_context: egui::Context,
+    pub egui_input_state: egui_glfw::EguiInputState,
+    pub start_time: Instant,
+}
+
+impl AppWindow {
+    const DEFAULT_WINDOW_HINTS: [WindowHint; 5] = [
+        glfw::WindowHint::ContextVersion(4, 3),
+        glfw::WindowHint::OpenGlProfile(glfw::OpenGlProfileHint::Core),
+        glfw::WindowHint::DoubleBuffer(true),
+        glfw::WindowHint::Resizable(true),
+        glfw::WindowHint::Samples(Some(8)),
+    ];
+
+    pub fn new_default(screen_width: u32, scree_height: u32) -> AppWindow {
+        let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+        Self::DEFAULT_WINDOW_HINTS.iter().for_each(|hint| {
+            glfw.window_hint(hint.clone());
+        });
+
+        let (mut window, events) = glfw
+            .create_window(
+                screen_width,
+                scree_height,
+                "Egui in GLFW!",
+                glfw::WindowMode::Windowed,
+            )
+            .expect("Failed to create GLFW window");
+
+        window.set_char_polling(true);
+        window.set_cursor_pos_polling(true);
+        window.set_key_polling(true);
+        window.set_mouse_button_polling(true);
+        window.set_size_polling(true);
+        window.make_current();
+
+        gl::load_with(|s| window.get_proc_address(s));
+        unsafe { gl::Enable(gl::MULTISAMPLE) };
+
+        let painter = egui_glfw::Painter::new(&mut window);
+        let egui_context = egui::Context::default();
+
+        let (width, height) = window.get_framebuffer_size();
+        let native_pixels_per_point = window.get_content_scale().0;
+
+        let egui_input_state = egui_glfw::EguiInputState::new(egui::RawInput {
+            screen_rect: Some(Rect::from_min_size(
+                Pos2::new(0f32, 0f32),
+                vec2(width as f32, height as f32) / native_pixels_per_point,
+            )),
+            ..Default::default() // todo: add pixels_per_point
+        });
+
+        AppWindow {
+            glfw,
+            window,
+            events,
+            painter,
+            egui_context,
+            egui_input_state,
+            start_time: Instant::now(),
+        }
+    }
+
+    pub fn get_window_mut(&mut self) -> &mut glfw::Window {
+        &mut self.window
+    }
+
+    pub fn get_window(&self) -> &glfw::Window {
+        &self.window
+    }
+
+    pub fn begin_frame(&mut self) {
+        self.egui_input_state.input.time = Some(self.start_time.elapsed().as_secs_f64());
+        self.egui_context
+            .begin_frame(self.egui_input_state.input.take());
+    }
+
+    pub fn end_frame(&mut self) {
+        let native_pixels_per_point = self.window.get_content_scale().0;
+        self.egui_context
+            .set_pixels_per_point(native_pixels_per_point);
+        let egui::FullOutput {
+            platform_output,
+            textures_delta,
+            shapes,
+            pixels_per_point: native_pixels_per_point,
+            viewport_output: _,
+        } = self.egui_context.end_frame();
+
+        //Handle cut, copy text from egui
+        if !platform_output.copied_text.is_empty() {
+            egui_glfw::copy_to_clipboard(&mut self.egui_input_state, platform_output.copied_text);
+        }
+
+        //Note: passing a bg_color to paint_jobs will clear any previously drawn stuff.
+        //Use this only if egui is being used for all drawing and you aren't mixing your own Open GL
+        //drawing calls with it.
+        let clipped_shapes: Vec<egui::ClippedPrimitive> = self
+            .egui_context
+            .tessellate(shapes, native_pixels_per_point);
+
+        self.painter
+            .paint_and_update_textures(1.0, &clipped_shapes, &textures_delta);
+
+        for (_, event) in glfw::flush_messages(&self.events) {
+            match event {
+                glfw::WindowEvent::Close => self.window.set_should_close(true),
+                glfw::WindowEvent::Size(width, height) => {
+                    self.painter.set_size(width as _, height as _);
+                    egui_glfw::handle_event(event, &mut self.egui_input_state);
+                }
+                glfw::WindowEvent::ContentScale(x, y) => {
+                    print!("{} {}", x, y)
+                }
+                glfw::WindowEvent::FramebufferSize(x, y) => {
+                    print!("{} {}", x, y)
+                }
+                _ => {
+                    // println!("{:?}", event);
+                    egui_glfw::handle_event(event, &mut self.egui_input_state);
+                }
+            }
+        }
+
+        self.window.swap_buffers();
+        self.glfw.poll_events();
+    }
+
+    pub fn get_egui_context(&self) -> &egui::Context {
+        &self.egui_context
+    }
+}
+
 fn main() {
-    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    glfw.window_hint(glfw::WindowHint::ContextVersion(3, 2));
-    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
-        glfw::OpenGlProfileHint::Core,
-    ));
-    glfw.window_hint(glfw::WindowHint::DoubleBuffer(true));
-    glfw.window_hint(glfw::WindowHint::Resizable(false));
-
-    let (mut window, events) = glfw
-        .create_window(
-            SCREEN_WIDTH,
-            SCREEN_HEIGHT,
-            "Egui in GLFW!",
-            glfw::WindowMode::Windowed,
-        )
-        .expect("Failed to create GLFW window.");
-
-    window.set_char_polling(true);
-    window.set_cursor_pos_polling(true);
-    window.set_key_polling(true);
-    window.set_mouse_button_polling(true);
-    window.make_current();
-    glfw.set_swap_interval(glfw::SwapInterval::Sync(1));
-
-    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
-
-    let mut painter = egui_glfw::Painter::new(&mut window);
-    let egui_ctx = egui::Context::default();
-
-    let (width, height) = window.get_framebuffer_size();
-    let native_pixels_per_point = window.get_content_scale().0;
-
-    let mut egui_input_state = egui_glfw::EguiInputState::new(egui::RawInput {
-        screen_rect: Some(Rect::from_min_size(
-            Pos2::new(0f32, 0f32),
-            vec2(width as f32, height as f32) / native_pixels_per_point,
-        )),
-        ..Default::default()
-    });
-
-    let start_time = Instant::now();
+    let mut app_window = AppWindow::new_default(SCREEN_WIDTH, SCREEN_HEIGHT);
 
     let mut test_str =
         "A text box to write in. Cut, copy, paste commands are available.".to_owned();
@@ -61,12 +163,13 @@ fn main() {
     let triangle_widget: TriangleWidget = triangle.into();
     let mut quit = false;
     let mut amplitude = 10.0;
-    while !window.should_close() {
-        egui_input_state.input.time = Some(start_time.elapsed().as_secs_f64());
-        egui_ctx.begin_frame(egui_input_state.input.take());
 
-        egui::Window::new("Egui with GLFW").show(&egui_ctx, |ui| {
-            egui::TopBottomPanel::top("Top").show(&egui_ctx, |ui| {
+    while !app_window.get_window().should_close() {
+        app_window.begin_frame();
+        let egui_context = app_window.get_egui_context();
+
+        egui::Window::new("Egui with GLFW").show(egui_context, |ui| {
+            egui::TopBottomPanel::top("Top").show(egui_context, |ui| {
                 ui.menu_button("File", |ui| {
                     {
                         let _ = ui.button("test 1");
@@ -88,39 +191,9 @@ fn main() {
             }
         });
 
-        egui::CentralPanel::default().show(&egui_ctx, |ui| ui.add(triangle_widget.clone()));
+        egui::CentralPanel::default().show(egui_context, |ui| ui.add(triangle_widget.clone()));
 
-        let egui::FullOutput {
-            platform_output,
-            textures_delta,
-            shapes,
-            pixels_per_point: native_pixels_per_point,
-            viewport_output: _,
-        } = egui_ctx.end_frame();
-
-        //Handle cut, copy text from egui
-        if !platform_output.copied_text.is_empty() {
-            egui_glfw::copy_to_clipboard(&mut egui_input_state, platform_output.copied_text);
-        }
-
-        //Note: passing a bg_color to paint_jobs will clear any previously drawn stuff.
-        //Use this only if egui is being used for all drawing and you aren't mixing your own Open GL
-        //drawing calls with it.
-        //Since we are custom drawing an OpenGL Triangle we don't need egui to clear the background.
-
-        let clipped_shapes = egui_ctx.tessellate(shapes, native_pixels_per_point);
-        painter.paint_and_update_textures(1.0, &clipped_shapes, &textures_delta);
-
-        for (_, event) in glfw::flush_messages(&events) {
-            match event {
-                glfw::WindowEvent::Close => window.set_should_close(true),
-                _ => {
-                    egui_glfw::handle_event(event, &mut egui_input_state);
-                }
-            }
-        }
-        window.swap_buffers();
-        glfw.poll_events();
+        app_window.end_frame();
 
         if quit {
             break;
